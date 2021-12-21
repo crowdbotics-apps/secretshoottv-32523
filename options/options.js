058b7317de15c3a9e7a4243976cbf36e0685b169
Crowdbotics <team@crowdbotics.com>
2021-12-21 18:15:41 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "secretshoottv_32523",
+  url: "https://secretshoottv_32523.botics.co",
+  api: "https://secretshoottv_32523.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
